edfb8c47589fa84aedab572ec25bf5c40ae52acb
rubzk <tomas.ertola@gmail.com>
2023-03-06 16:25:21 +0100

    New design of main page, added animation behind the picture

--- themes/basic/tailwind.config.js
+++ themes/basic/tailwind.config.js
@@ -15,9 +15,49 @@ module.exports = {
 
       fontFamily: {
         ...fontFamily,
-        'sans': 'Helvetica, sans-serif'
+        'sans': 'Monserrat, sans-serif'
       },
-
+      maxWidth: {
+        '96': '24rem',
+      },
+      maxHeight: {
+        '96': '24rem',
+      },
+      animation: {
+        blob: "blob 7s infinite",
+        cursor: 'cursor .6s linear infinite alternate',
+        type: 'type 1.8s ease-out .8s 1 normal both',
+        'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px,0px) scale(1)"
+          },
+          "33%": {
+            transform: "translate(30px,-50px) scale(1.1)"
+          },
+          "66%": {
+            transform: "translate(-20px,20px) scale(0.9)"
+          },
+          "100%": {
+            transform: "translate(0px,0px) scale(1)"
+          }
+        },
+        type: {
+          '0%': { width: '0ch' },
+          '5%, 10%': { width: '1ch' },
+          '15%, 20%': { width: '2ch' },
+          '25%, 30%': { width: '3ch' },
+          '35%, 40%': { width: '4ch' },
+          '45%, 50%': { width: '5ch' },
+          '55%, 60%': { width: '6ch' },
+          '65%, 70%': { width: '7ch' },
+          '75%, 80%': { width: '8ch' },
+          '85%, 90%': { width: '9ch' },
+          '95%': { width: '10ch' },
+        }
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
